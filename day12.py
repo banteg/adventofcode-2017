3cabd92d9aaa50ef6dab21c2cb369b3789ba4b62
banteg <banteeg@gmail.com>
2019-05-18 17:52:42 +0700

    add day 12 part 2

--- day12.py
+++ day12.py
@@ -2,9 +2,7 @@ import networkx as nx
 
 import aoc
 
-
-@aoc.test({
-    '''
+example = '''
     0 <-> 2
     1 <-> 1
     2 <-> 0, 3, 4
@@ -12,8 +10,10 @@ import aoc
     4 <-> 2, 3, 6
     5 <-> 6
     6 <-> 4, 5
-    ''': 6
-})
+    '''
+
+
+@aoc.test({example: 6})
 def part_1(data: aoc.Data):
     G = nx.Graph()
     for node, *edges in data.ints_lines:
@@ -22,3 +22,11 @@ def part_1(data: aoc.Data):
         if 0 in component:
             nx.nx_agraph.write_dot(G.subgraph(component), 'pipes_zero.dot')
             return len(component)
+
+
+@aoc.test({example: 2})
+def part_2(data: aoc.Data):
+    G = nx.Graph()
+    for node, *edges in data.ints_lines:
+        G.add_edges_from((node, edge) for edge in edges)
+    return len(list(nx.connected_components(G)))
