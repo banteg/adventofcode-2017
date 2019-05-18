932aca208af2c64a413137d8eee5861436825dac
banteg <banteeg@gmail.com>
2019-05-18 15:55:47 +0700

    add day 09 part 2

--- day09.py
+++ day09.py
@@ -1,19 +1,10 @@
 import aoc
 
 
-@aoc.test({
-    r'{}': 1,
-    r'{{{}}}': 6,
-    r'{{},{}}': 5,
-    r'{{{},{},{{}}}}': 16,
-    r'{<a>,<a>,<a>,<a>}': 1,
-    r'{{<ab>},{<ab>},{<ab>},{<ab>}}': 9,
-    r'{{<!!>},{<!!>},{<!!>},{<!!>}}': 9,
-    r'{{<a!>},{<a!>},{<a!>},{<ab>}}': 3,
-})
-def part_1(data: aoc.Data):
+def process_stream(data, part=1):
     groups = []
     garbage = []
+    collected = []
     score = 0
     ignore = False
     for char in data:
@@ -30,6 +21,36 @@ def part_1(data: aoc.Data):
                 groups.append(len(groups) + 1)
             if char == '}':
                 score += groups.pop()
-        elif char == '>':
-            garbage.pop()
-    return score
+        else:
+            if char == '>':
+                garbage.pop()
+            else:
+                collected.append(char)
+    return {1: score, 2: len(collected)}[part]
+
+
+@aoc.test({
+    r'{}': 1,
+    r'{{{}}}': 6,
+    r'{{},{}}': 5,
+    r'{{{},{},{{}}}}': 16,
+    r'{<a>,<a>,<a>,<a>}': 1,
+    r'{{<ab>},{<ab>},{<ab>},{<ab>}}': 9,
+    r'{{<!!>},{<!!>},{<!!>},{<!!>}}': 9,
+    r'{{<a!>},{<a!>},{<a!>},{<ab>}}': 3,
+})
+def part_1(data: aoc.Data):
+    return process_stream(data, part=1)
+
+
+@aoc.test({
+    r'<>': 0,
+    r'<random characters>': 17,
+    r'<<<<>': 3,
+    r'<{!>}>': 2,
+    r'<!!>': 0,
+    r'<!!!>>': 0,
+    r'<{o"i!a,<{i<a>': 10,
+})
+def part_2(data: aoc.Data):
+    return process_stream(data, part=2)
