eabbf2ce04484f044fc1c3da7aeaa8e2ae8366fc
banteg <banteeg@gmail.com>
2019-05-18 15:08:55 +0700

    format day 07

--- day07.py
+++ day07.py
@@ -4,19 +4,21 @@ from statistics import median
 from collections import Counter
 
 
-example = '''pbga (66)
-xhth (57)
-ebii (61)
-havc (66)
-ktlj (57)
-fwft (72) -> ktlj, cntj, xhth
-qoyq (66)
-padx (45) -> pbga, havc, qoyq
-tknk (41) -> ugml, padx, fwft
-jptl (61)
-ugml (68) -> gyxo, ebii, jptl
-gyxo (61)
-cntj (57)'''
+example = '''
+    pbga (66)
+    xhth (57)
+    ebii (61)
+    havc (66)
+    ktlj (57)
+    fwft (72) -> ktlj, cntj, xhth
+    qoyq (66)
+    padx (45) -> pbga, havc, qoyq
+    tknk (41) -> ugml, padx, fwft
+    jptl (61)
+    ugml (68) -> gyxo, ebii, jptl
+    gyxo (61)
+    cntj (57)
+    '''
 
 
 def load_programs(data):
